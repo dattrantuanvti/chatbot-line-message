d826b6eb32a15476c482ed2a2f23343977e76ca8
dat.trantuan <dat.trantuan@vti.com.vn>
2024-01-31 14:47:34 +0700

    update

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
-const express = require("express");
 const line = require("@line/bot-sdk");
+const express = require("express");
 require("dotenv").config();
 
+// create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 };
 
-const app = express();
-app.post("/webhook", line.middleware(config), (req, res) => {
-  Promise.all(req.body.events.map(handleEvent)).then((result) =>
-    res.json(result)
-  );
-});
-
+// create LINE SDK client
 const client = new line.messagingApi.MessagingApiClient({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
 });
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post("/callback", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    status: true,
+  });
+});
+
+// event handler
 function handleEvent(event) {
   if (event.type !== "message" || event.message.type !== "text") {
+    // ignore non-text-message event
     return Promise.resolve(null);
   }
 
+  // create an echoing text message
+  const echo = { type: "text", text: event.message.text };
+
+  // use reply API
   return client.replyMessage({
     replyToken: event.replyToken,
-    messages: [
-      {
-        type: "text",
-        text: event.message.text,
-      },
-    ],
+    messages: [echo],
   });
 }
 
-app.listen(8000);
+// listen on port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
