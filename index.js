12ef905d2cfa8491271429f2411fd67b6a8df7fa
dat.trantuan <dat.trantuan@vti.com.vn>
2024-01-31 14:21:31 +0700

    update

--- index.js
+++ index.js
@@ -1,57 +1,54 @@
+require("dotenv").config();
 const express = require("express");
-const middleware = require("@line/bot-sdk").middleware;
 const JSONParseError = require("@line/bot-sdk").JSONParseError;
 const SignatureValidationFailed =
   require("@line/bot-sdk").SignatureValidationFailed;
+const MessagingApiClient =
+  require("@line/bot-sdk").messagingApi.MessagingApiClient;
 
 const app = express();
 
-const config = {
-  channelAccessToken: "YOUR_CHANNEL_ACCESS_TOKEN",
-  channelSecret: "YOUR_CHANNEL_SECRET",
-};
-
-const client = new line.Client(lineConfig);
-app.use(middleware(config));
+const client = new MessagingApiClient({
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+});
 
 app.post("/webhook", (req, res) => {
   const event = req.body.events[0];
 
-  switch (event.type) {
-    case "join":
-    case "follow":
-      return client.replyMessage(event.replyToken, {
-        type: "text",
-        text: "Hello, Wellcome you!",
-      });
-    case "message":
-      switch (event.message.type) {
-        case "text":
-          switch (event.message.text) {
-            case "hello":
-              return client.replyMessage(event.replyToken, {
-                type: "text",
-                text: "Can we help you?",
-              });
-            case "I want book ticket":
-              return client.replyMessage(event.replyToken, {
-                type: "text",
-                text: "Where do you want to book tickets?",
-              });
-          }
+  if (event.type === "message") {
+    const message = event.message;
+    if (message.type === "text" && message.text === "bye") {
+      if (event.source.type === "room") {
+        client.leaveRoom(event.source.roomId);
+      } else if (event.source.type === "group") {
+        client.leaveGroup(event.source.groupId);
+      } else {
+        client.replyMessage({
+          replyToken: event.replyToken,
+          messages: [
+            {
+              type: "text",
+              text: "Nhân viên sẽ hỗ trợ bạn trong ít phút!",
+            },
+          ],
+        });
       }
+    }
   }
+  return res.json(req.body.events); // req.body will be webhook event object
 });
 
+// Handle errors
 app.use((err, req, res, next) => {
   if (err instanceof SignatureValidationFailed) {
-    res.status(401).send(err.signature);
-    return;
+    return res.status(401).send(err.signature);
   } else if (err instanceof JSONParseError) {
-    res.status(400).send(err.raw);
-    return;
+    return res.status(400).send(err.raw);
   }
-  next(err); // will throw default 500
+  next(err);
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+  console.log("App is listening on PORT " + PORT);
+});
