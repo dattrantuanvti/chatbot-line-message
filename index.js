17a958470022963bbf6ef959e473635b81e3b814
dat.trantuan <dat.trantuan@vti.com.vn>
2024-01-31 15:13:30 +0700

    update

--- index.js
+++ index.js
@@ -10,10 +10,30 @@ const config = {
 const app = express();
 
 app.post("/webhook", middleware(config), (req, res) => {
-  res.status(200).json({
-    a: req.body.events,
-    b: req.body.destination,
-  });
+  const event = req.body.events[0];
+
+  if (event.type === "message") {
+    const message = event.message;
+
+    if (message.type === "text" && message.text === "bye") {
+      if (event.source.type === "room") {
+        client.leaveRoom(event.source.roomId);
+      } else if (event.source.type === "group") {
+        client.leaveGroup(event.source.groupId);
+      } else {
+        client.replyMessage({
+          replyToken: event.replyToken,
+          messages: [
+            {
+              type: "text",
+              text: "I cannot leave a 1-on-1 chat!",
+            },
+          ],
+        });
+      }
+    }
+  }
+  return res.status(200).json({ event });
 });
 
 // listen on port
