af107161b0ff87db1a2c9a61f250e56bb3e72876
dat.trantuan <dat.trantuan@vti.com.vn>
2024-01-31 15:35:12 +0700

    update

--- index.js
+++ index.js
@@ -12,38 +12,18 @@ const client = new line.messagingApi.MessagingApiClient({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
 });
 
-// event handler
-function handleEvent(event) {
-  if (event.type !== "message" || event.message.type !== "text") {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
-
-  // create an echoing text message
-  const echo = { type: "text", text: event.message.text };
-
-  // use reply API
-  return client.replyMessage({
-    replyToken: event.replyToken,
-    messages: [echo],
-  });
-}
-
 const app = express();
 
 app.post("/webhook", middleware(config), (req, res) => {
   Promise.all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
+    .then((result) => {
+      console.log(result);
+      res.json(result);
+    })
     .catch((err) => {
       console.error(err);
       res.status(500).end();
     });
-  res.status(200).json({
-    a: req.body.events,
-    b: req.body.destination,
-    c: req.body,
-    res: res,
-  });
 });
 
 // listen on port
